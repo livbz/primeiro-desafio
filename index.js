3fe1c08a6272f5ca95b1409844b0094167b44657
livbz <liviabarbozasc9@gmail.com>
2024-03-20 20:42:38 -0300

    a jornada da heroína V

---
+++ index.js
@@ -0,0 +1,22 @@
+let nomeDaHeroina = "V"
+let xp = 10000
+
+if (xp < 1000) {
+    console.log(xp ="ferro")
+}else if (xp >= 1001 && xp <= 2000) {
+    console.log(xp ="bronze")
+}else if (xp >= 2001 && xp <= 5000){
+    console.log(xp ="prata")
+}else if (xp >= 5001 && xp <= 7000){
+    console.log(xp ="ouro")
+}else if (xp >= 7001 && xp <= 8000){
+    console.log(xp ="platina")
+}else if (xp >= 8001 && xp <= 9000){
+    console.log(xp ="ascendente")
+}else if (xp >= 9001 && xp <= 10000){
+    console.log(xp ="imortal")
+}else if (xp >= 10001 && xp <= 10001){
+    console.log(xp ="radiante")
+}
+
+console.log("A heroína de nome " + nomeDaHeroina + " está no nível de " + xp)
